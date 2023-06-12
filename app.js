a4eea71c83ad728e9b0896e3af5e192cf3c51217
phungle <phungple2015@gmail.com>
2023-06-12 16:34:14 -0500

    import inquirer package into the project

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-const fs = require('fs'); // need this statement to use the fs module
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+import inquirer from "inquirer";
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+//const pageHTML = generatePage(names, github);
+
+//const profileDataArgs = process.argv.slice(2);
 
 /*const names = profileDataArgs[0];
 const github = profileDataArgs[1];
 we can use an ES6 feature called assignment destructuring to assigns above elements of profileDataArgs
-array to variable names in a single expression */
-const [names, github] = profileDataArgs;
+array to variable names in a single expression 
+const [names, github] = profileDataArgs; */
 
-fs.writeFile('./index.html', generatePage(names, github), err => {
+//fs.writeFile('./index.html', pageHTML, err => {
     /* In the callback function block, a conditional statement checks for the err being returned by the callback function. 
     If err exists, an error message is displayed, using the following statement: */
-    if (err) throw new Error(err);
+//    if (err) throw err;
     /* Rather than silently displaying the error with console.log(err);, 
     the preceding statement creates an exception and stops the execution of the code.*/
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-/* console.log(names, github);
-console.log(generatePage(names, github)); */
+
 
 
 
