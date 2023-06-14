e85a04357f8f58cd07191ec04bf7f10039b19568
phungle <phungple2015@gmail.com>
2023-06-14 13:46:43 -0500

    create project section html

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import inquirer from "inquirer";
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
-//const pageHTML = generatePage(names, github);
+import fs from 'fs';
+import generatePage from './src/page-template.js';
 
 //const profileDataArgs = process.argv.slice(2);
 
@@ -11,14 +10,6 @@ we can use an ES6 feature called assignment destructuring to assigns above eleme
 array to variable names in a single expression 
 const [names, github] = profileDataArgs; */
 
-//fs.writeFile('./index.html', pageHTML, err => {
-    /* In the callback function block, a conditional statement checks for the err being returned by the callback function. 
-    If err exists, an error message is displayed, using the following statement: */
-//    if (err) throw err;
-    /* Rather than silently displaying the error with console.log(err);, 
-    the preceding statement creates an exception and stops the execution of the code.*/
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//});
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -72,10 +63,10 @@ const promptUser = () => {
 
 const promptProject = portfolioData => {
     console.log(`
-=================
-Add a New Project
-=================
-`);
+        =================
+        Add a New Project
+        =================
+    `);
     // to avoid the projects array to be set to an empty array in every function call
     // If there's no 'projects' array property, create one
     if (!portfolioData.projects) {
@@ -152,12 +143,71 @@ Add a New Project
             }
         });
 };
-
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
+const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    confirmAbout: true,
+    about:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+    projects: [
+        {
+            name: 'Run Buddy',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['HTML', 'CSS'],
+            link: 'https://github.com/lernantino/run-buddy',
+            feature: true,
+            confirmAddProject: true
+        },
+        {
+            name: 'Taskinator',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'HTML', 'CSS'],
+            link: 'https://github.com/lernantino/taskinator',
+            feature: true,
+            confirmAddProject: true
+        },
+        {
+            name: 'Taskmaster Pro',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+            link: 'https://github.com/lernantino/taskmaster-pro',
+            feature: false,
+            confirmAddProject: true
+        },
+        {
+            name: 'Robot Gladiators',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+            languages: ['JavaScript'],
+            link: 'https://github.com/lernantino/robot-gladiators',
+            feature: false,
+            confirmAddProject: false
+        }
+    ]
+};
+    
+const pageHTML = generatePage(mockData);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+});
+//promptUser()
+//    .then(promptProject)
+//    .then(portfolioData => {
+//        const pageHTML = generatePage(portfolioData);
+        //fs.writeFile('./index.html', pageHTML, err => {
+            /* In the callback function block, a conditional statement checks for the err being returned by the callback function. 
+                If err exists, an error message is displayed, using the following statement.
+                Rather than silently displaying the error with console.log(err);, 
+                the statement below creates an exception and stops the execution of the code.
+            */
+            //if (err) throw new Error(err);
+            
+            //console.log('Page created! Check out index.html to see the output!');
+        //});
+//    });
     
 
 
